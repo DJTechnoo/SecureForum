cb533fe67fe077d3abd7f79e2f04bc9695f7e621
askelej <askelej@stud.ntnu.no>
2018-09-18 13:43:42 +0200

    login, extremely basic and poor and insecure

---
+++ View.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+
+<html>
+	<head> 
+		<title>BEST EVER</title>
+	</head>
+	
+	<body>
+		<form action="login.php" method="POST">
+	Username: <input type="Username" name="username"/><br>
+	Password: <input type="Password" name="password"/><br>
+	<input type="Submit" name="submit"/>
+	
+	</form>
+	</body>
+	
+
+</html>
---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+$username = $_POST['username'];
+
+echo "The username is $username <br>";
+
+?>
